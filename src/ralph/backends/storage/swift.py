0d73ee2ae640a1e4691594e01ec5fa557489eef4
SergioSim <Sergio.Simonian@etu.unice.fr>
2021-01-22 12:22:31 +0300

    WIP SwiftStorage read

--- src/ralph/backends/storage/swift.py
+++ src/ralph/backends/storage/swift.py
@@ -1,8 +1,11 @@
 """Swift storage backend for Ralph"""
 
+import datetime
 import inspect
 import logging
+import sys
 
+from swiftclient.exceptions import ClientException
 from swiftclient.service import SwiftError, SwiftService
 
 from ralph.defaults import (
@@ -17,6 +20,19 @@ from ..mixins import HistoryMixin
 from .base import BaseStorage
 
 logger = logging.getLogger(__name__)
+
+
+def throws_swift_error(func):
+    """Decorator catches SwiftError or ClientException, then logs and re-raises it"""
+
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (SwiftError, ClientException) as error:
+            logger.error(error.value)
+            raise error
+
+    return wrapper
 
 
 class SwiftStorage(HistoryMixin, BaseStorage):
@@ -41,6 +57,7 @@ class SwiftStorage(HistoryMixin, BaseStorage):
     ):
         """Setup options for the SwiftService"""
 
+        self.container = os_storage_url.split("/")[-1]
         self.options = {
             "os_auth_url": os_auth_url,
             "os_identity_api_version": os_identity_api_version,
@@ -51,24 +68,23 @@ class SwiftStorage(HistoryMixin, BaseStorage):
             "os_username": os_username,
             "os_password": os_password,
             "os_region_name": os_region_name,
-            "os_storage_url": os_storage_url,
+            "os_storage_url": os_storage_url.replace(f"/{self.container}", ""),
         }
-        self._check_required_options()
+        self._check_options()
 
-    def _check_required_options(self):
+    def _check_options(self):
         """Verify that all options are not None nor empty"""
 
         for option_name in self.options:
-            option_value = self.options[option_name]
+            option = self.options[option_name]
             # pylint: disable=protected-access
-            if option_value is not None and not isinstance(
-                option_value, inspect._empty
-            ):
+            if option is not None and not isinstance(option, inspect._empty):
                 continue
             raise BackendParameterException(
                 f"SwiftStorage backend instance requires the `{option_name}` option to be set."
             )
 
+    @throws_swift_error
     def list(self, details=False, new=False):
         """List files in the storage backend
 
@@ -83,39 +99,81 @@ class SwiftStorage(HistoryMixin, BaseStorage):
             {
                 "bytes": 459,
                 "last_modified": "2021-01-20T10:41:03.950030",
-                "hash": "01018bc73b8b3491c6c3d543044a702b",
+                "hash": "b10a8db164e0754105b7a99be72e3fe5",
                 "name": "2020-04-29.gz",
                 "content_type": "application/gzip"
             }
         """
-        try:
-            with SwiftService(options=self.options) as swift:
-                for page in swift.list():
-                    if not page["success"]:
-                        msg = "Failed to list container %s: %s"
-                        logger.error(msg, page["container"], page["error"])
+
+        archives_to_skip = set()
+        if new:
+            fetched_from_swift = (
+                lambda e: e["backend"] == self.name and e["command"] == "fetch"
+            )
+            for entry in filter(fetched_from_swift, self.history):
+                archives_to_skip.add(entry["id"])
+        with SwiftService(options=self.options) as swift:
+            for page in swift.list(container=self.container):
+                if not page["success"]:
+                    msg = "Failed to list container %s: %s"
+                    logger.error(msg, page["container"], page["error"])
+                    continue
+                for archive in page["listing"]:
+                    if (
+                        new
+                        and self._get_histrory_id(archive["name"]) in archives_to_skip
+                    ):
                         continue
-                    archives = page["listing"]
-                    if new:
-                        archives = set(archives) - set(
-                            entry.get("id")
-                            for entry in filter(
-                                lambda e: e["backend"] == self.name and e["command"] == "fetch",
-                                self.history,
-                            )
-                    )
-                    for archive in page["listing"]:
-                        yield archive if details else archive["name"]
-        except SwiftError as error:
-            logger.error(error.value)
-            raise error
+                    yield archive if details else archive["name"]
 
     def url(self, name):
         """Get `name` file absolute URL"""
 
-    def read(self, name, chunk_size=4096):
-        """Read `name` file and stream its content by chunks of a given size"""
-        logger.error("Read S")
+        # What's the purpose of this function ? Seems not used anywhere.
+        return f"{self.options.get('os_storage_url')}/{name}"
 
+    def _get_histrory_id(self, name):
+        return f"{self.container}/{name}"
+
+    @throws_swift_error
+    def read(self, name, chunk_size=None):
+        """Read `name` file and stream its content by chunks of (max) 2 ** 16
+        Not 100% sure why 2 ** 16 but may be because swift.service.py line 54 is:
+            DISK_BUFFER = 2 ** 16 (TODO: Check this to be sure)
+        """
+
+        logger.debug("Getting archive: %s", name)
+
+        with SwiftService(options=self.options) as swift:
+            options = {"options": {"out_file": "-"}}
+            download = next(
+                swift.download(container=self.container, objects=[name], **options)
+            )
+            if "contents" not in download:
+                raise BackendParameterException(
+                    f"Failed to download {download['object']}, {download['error']}"
+                )
+            size = 0
+            for chunk in download["contents"]:
+                logger.debug("Chunk %s", len(chunk))
+                size += len(chunk)
+                sys.stdout.buffer.write(chunk)
+
+            # Archive is supposed to have been fully fetched, add a new entry to
+            # the history.
+            self.append_to_history(
+                {
+                    "backend": self.name,
+                    "command": "fetch",
+                    "id": self._get_histrory_id(name),
+                    "filename": name,
+                    "size": size,
+                    "fetched_at": datetime.datetime.now(
+                        tz=datetime.timezone.utc
+                    ).isoformat(),
+                }
+            )
+
+    @throws_swift_error
     def write(self, name, chunk_size=4096, overwrite=False):
         """Write content to the `name` target"""
